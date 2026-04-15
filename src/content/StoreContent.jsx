22616a3778e83eca6e93658be8a78b2345da71f4
Youcancallmefah <napatsawan.san18@gmail.com>
2026-04-15 22:54:40 +0700

    feat: implement functional cart system with context

--- src/content/StoreContent.jsx
+++ src/content/StoreContent.jsx
@@ -1,9 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) =>{
-    const {cartItems,setCartItems} = useState({});
+    const [cartItems,setCartItems] = useState({});
     const addToCart =(itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -12,11 +12,20 @@ const StoreContextProvider = (props) =>{
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         }
     }
-    const removwFromCart = (itemId) => {
+    const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
+
     const contextValue ={
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     
     return (
